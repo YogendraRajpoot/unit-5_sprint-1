f441b0f16db36648d446bfa1bad627afc04f90e7
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-01-31 21:05:49 +0530

    third commit

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ class rectangle{
     constructor(len,bre){
         this.len = len,
         this.bre = bre
+        console.log(`\nlength=${this.len} \nwidth=${this.bre}`)
     }
     area(){
-        return (this.len*this.bre)
+        return `AREA = ${(this.len*this.bre)}`
     }
+    
 }
 class square extends rectangle {
-    constructor(len ,bre){
-        super(len,bre)
+    constructor(len){
+        super(len,len)
     }
     area(){
-        super.area()
+        return `AREA = ${(this.len*this.len)}`
+    }
+    perimeter(){
+        return `PERIMETER = ${2*(this.len +this.len)}`
     }
 }
 
 var rectangle1 = new rectangle(2,4)
 console.log(rectangle1.area())
+
+var square1 = new square(8)
+console.log(square1.area())
+
+var square2 = new square(9)
+console.log(square2.perimeter())
